3e7a8b2c0a17309076ae4d98b0cfa61876072e48
Raqeeb Uddin <ruddin@aiimi.com>
2018-07-19 15:43:38 +0100

    Add project files.

---
+++ ElasticsearchIntergationApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using ElasticsearchIntergationApplication.Model;
+using Nest;
+using Newtonsoft;
+
+namespace ElasticsearchIntergationApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("people");
+            var client = new ElasticClient(settings);
+
+            //index a single poco
+
+            var person = new Person
+            {
+                FirstName = "Martijn1",
+                LastName = "Laarman"
+            };
+
+            var indexResponse = client.IndexDocument(person);
+
+
+        }
+    }
+}
